bf1545434ed50b79f66704e89384c30c739bdc04
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-23 18:59:44 -0400

    added main entrypoint of the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,23 @@
-package server
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"tonotdolist/cmd/server/wire"
+)
 
 func main() {
+	app := wire.InitializeApp()
+	app.Start()
 
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	app.Stop(ctx)
 }
