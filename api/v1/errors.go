c0b7aac90a1a6d925fcb172fd01365f15de017e1
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-24 16:01:29 -0400

    added api v1 response protocol

---
+++ api/v1/errors.go
@@ -0,0 +1,9 @@
+package v1
+
+var (
+	ErrSuccess             = newError(200, 200, "Ok")
+	ErrBadRequest          = newError(400, 400, "Bad Request")
+	ErrUnauthorized        = newError(401, 401, "Unauthorized")
+	ErrNotFound            = newError(404, 404, "Not Found")
+	ErrInternalServerError = newError(500, 500, "Internal Server Error")
+)
---
+++ api/v1/v1.go
@@ -0,0 +1,34 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func HandleSuccess(ctx *gin.Context, data interface{}) {
+	HandleError(ctx, ErrSuccess, data)
+}
+
+func HandleError(ctx *gin.Context, err *Error, data interface{}) {
+	if data == nil {
+		data = map[string]string{}
+	}
+
+	resp := Response{Code: err.Code, Message: err.Message, Data: data}
+	ctx.JSON(err.HTTPCode, resp)
+}
+
+type Error struct {
+	HTTPCode int
+	Code     int
+	Message  string
+}
+
+func newError(httpCode int, code int, msg string) *Error {
+	return &Error{httpCode, code, msg}
+}
