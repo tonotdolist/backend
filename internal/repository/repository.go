9e724ac9fd852b5060d099c10590c256a6866214
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-25 11:26:07 -0400

    added initializer for base repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -9,3 +9,10 @@ type Repository struct {
 	logger zerolog.Logger
 	db     *gorm.DB
 }
+
+func NewRepository(logger zerolog.Logger, db *gorm.DB) *Repository {
+	return &Repository{
+		logger: logger,
+		db:     db,
+	}
+}
