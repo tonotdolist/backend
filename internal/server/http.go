e82a8fbc9ae1377c83e7098ac56a258d41e7f4b3
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-27 18:48:35 -0400

    made http server take host and port from config

--- internal/server/http.go
+++ internal/server/http.go
@@ -3,11 +3,22 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
+	"github.com/spf13/viper"
+	"tonotdolist/pkg/config"
 	"tonotdolist/pkg/server/http"
 )
 
-func NewHTTPServer(logger zerolog.Logger) *http.Server {
-	s := http.NewServer(gin.Default(), logger, http.WithHost("0.0.0.0"), http.WithPort(8080))
+const (
+	HTTPHostKey = "server.http.host"
+	HTTPPortKey = "server.http.port"
+)
+
+func init() {
+	config.RegisterRequiredKey(HTTPHostKey, HTTPPortKey)
+}
+
+func NewHTTPServer(logger zerolog.Logger, viper *viper.Viper) *http.Server {
+	s := http.NewServer(gin.Default(), logger, http.WithHost(viper.GetString(HTTPHostKey)), http.WithPort(viper.GetUint16(HTTPPortKey)))
 
 	return s
 }
