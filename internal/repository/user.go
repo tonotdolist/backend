da43ff8f284889507b771d617f91cc26534dc25f
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-27 19:20:01 -0400

    added user repo with initializer; methods WIP

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,10 +2,31 @@ package repository
 
 import (
 	"context"
+	"github.com/rs/zerolog"
 	"tonotdolist/internal/model"
 )
 
 type UserRepository interface {
 	GetByEmail(context.Context, string) (*model.User, error)
 	CreateUser(context.Context, *model.User) error
+}
+
+type userRepository struct {
+	logger zerolog.Logger
+	*Repository
+}
+
+func NewUserRepository(repository *Repository) UserRepository {
+	return &userRepository{
+		Repository: repository,
+		logger:     repository.logger.With().Str("repository", "user").Logger(),
+	}
+}
+
+func (up *userRepository) GetByEmail(ctx context.Context, email string) (*model.User, error) {
+	panic("implement me")
+}
+
+func (up *userRepository) CreateUser(context.Context, *model.User) error {
+	panic("implement me")
 }
