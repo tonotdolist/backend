49f4f4f7871827b4183a532a3bd21cff9d6a42ed
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-29 00:54:06 -0400

    added login request handler

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,19 +1,56 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"reflect"
+	"tonotdolist/common"
+	"tonotdolist/internal/service"
+	"tonotdolist/pkg/api"
+	"tonotdolist/pkg/log"
+)
+
+var (
+	loginRequestType    = reflect.TypeOf(common.UserLoginRequest{})
+	registerRequestType = reflect.TypeOf(common.UserRegisterRequest{})
+)
 
 type UserHandler struct {
 	*Handler
+	userService service.UserService
 }
 
-func NewUserHandler(handler *Handler) *UserHandler {
+func NewUserHandler(handler *Handler, userService service.UserService) *UserHandler {
 	return &UserHandler{
-		Handler: handler,
+		Handler:     handler,
+		userService: userService,
 	}
 }
 
 func (h *UserHandler) Login(ctx *gin.Context) {
-	panic("implement me")
+	logger := log.GetLoggerFromContext(ctx)
+	version := ctx.GetUint("version")
+
+	rawReq, err := api.BindJSON(ctx, version, loginRequestType)
+
+	defer h.responder.HandleResponse(version, ctx, err, nil)
+
+	if err != nil {
+		if !errors.Is(err, common.ErrBadRequest) {
+			logger.Error().Err(err).Msg("unable to bind json")
+		}
+
+		return
+	}
+
+	req := rawReq.(*common.UserLoginRequest)
+	err = h.userService.Login(ctx, req)
+
+	if err != nil {
+		if !errors.Is(err, common.ErrUnauthorized) {
+			logger.Error().Err(err).Msg("error handling login request")
+		}
+	}
 }
 
 func (h *UserHandler) Register(ctx *gin.Context) {
