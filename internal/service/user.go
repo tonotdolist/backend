1c5f2f061203da5efe40d978aba0f91c426c773f
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-28 22:34:36 -0400

    added userService implementation; methods WIP

--- internal/service/user.go
+++ internal/service/user.go
@@ -3,9 +3,28 @@ package service
 import (
 	"context"
 	"tonotdolist/common"
+	"tonotdolist/internal/repository"
 )
 
 type UserService interface {
 	Login(context.Context, *common.UserLoginRequest) error
 	Register(context.Context, *common.UserRegisterRequest) error
+}
+
+type userService struct {
+	userRepository repository.UserRepository
+}
+
+func NewUserService(userRepository repository.UserRepository) UserService {
+	return &userService{
+		userRepository: userRepository,
+	}
+}
+
+func (s *userService) Login(ctx context.Context, req *common.UserLoginRequest) error {
+	panic("implement me")
+}
+
+func (s *userService) Register(ctx context.Context, req *common.UserRegisterRequest) error {
+	panic("implement me")
 }
