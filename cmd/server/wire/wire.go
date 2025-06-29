5d06aa6719881556e6effdcbc6d760e1feb7fdfd
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-29 16:22:57 -0400

    added user service, repository, and handler to wire

--- cmd/server/wire/wire.go
+++ cmd/server/wire/wire.go
@@ -8,25 +8,35 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/spf13/viper"
 	"tonotdolist/internal/app"
+	"tonotdolist/internal/handler"
+	"tonotdolist/internal/repository"
 	"tonotdolist/internal/server"
+	"tonotdolist/internal/service"
+	"tonotdolist/pkg/log"
 )
 
-//var repositorySet = wire.NewSet(
-//	repository.NewDB,
-//	repository.NewRepository,
-//	)
-//
-//var serviceSet = wire.NewSet()
-//
-//var handlerSet = wire.NewSet()
+var repositorySet = wire.NewSet(
+	repository.NewDB,
+	repository.NewRepository,
+	repository.NewUserRepository,
+)
+
+var serviceSet = wire.NewSet(
+	service.NewUserService,
+)
+
+var handlerSet = wire.NewSet(
+	handler.NewHandler,
+	handler.NewUserHandler,
+)
 
 func InitializeApp(logger zerolog.Logger, conf *viper.Viper) *app.App {
 	panic(wire.Build(
-		//log.NewGormLogger,
+		log.NewGormLogger,
 
-		//repositorySet,
-		//serviceSet,
-		//handlerSet,
+		repositorySet,
+		serviceSet,
+		handlerSet,
 
 		server.NewHTTPServer,
 
