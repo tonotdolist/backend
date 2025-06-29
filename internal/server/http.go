e7f593ba6e19bc504765a5eb6594c3f7eb52f0ea
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-29 16:21:07 -0400

    added login and register handler to http endpoint

--- internal/server/http.go
+++ internal/server/http.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 	"github.com/spf13/viper"
+	"tonotdolist/internal/handler"
 	"tonotdolist/pkg/config"
 	"tonotdolist/pkg/server/http"
 )
@@ -17,8 +18,15 @@ func init() {
 	config.RegisterRequiredKey(HTTPHostKey, HTTPPortKey)
 }
 
-func NewHTTPServer(logger zerolog.Logger, viper *viper.Viper) *http.Server {
+func NewHTTPServer(logger zerolog.Logger, viper *viper.Viper, userHandler *handler.UserHandler) *http.Server {
 	s := http.NewServer(gin.Default(), logger, http.WithHost(viper.GetString(HTTPHostKey)), http.WithPort(viper.GetUint16(HTTPPortKey)))
+
+	v1 := s.Group("/v1")
+	{
+		noAuth := v1.Group("/")
+		noAuth.POST("login", userHandler.Login)
+		noAuth.POST("register", userHandler.Register)
+	}
 
 	return s
 }
