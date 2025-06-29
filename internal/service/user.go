4ff417c85eb41380e3e35d4c3c6edd1f7caec705
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-28 23:10:49 -0400

    added login and register function to user service

--- internal/service/user.go
+++ internal/service/user.go
@@ -2,9 +2,21 @@ package service
 
 import (
 	"context"
+	"errors"
+	"github.com/spf13/viper"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 	"tonotdolist/common"
+	"tonotdolist/internal/model"
 	"tonotdolist/internal/repository"
+	"tonotdolist/pkg/config"
 )
+
+const bcryptCostKey = "auth.bcryptcost"
+
+func init() {
+	config.RegisterRequiredKey(bcryptCostKey)
+}
 
 type UserService interface {
 	Login(context.Context, *common.UserLoginRequest) error
@@ -12,19 +24,54 @@ type UserService interface {
 }
 
 type userService struct {
+	bcryptCost     int
 	userRepository repository.UserRepository
 }
 
-func NewUserService(userRepository repository.UserRepository) UserService {
+func NewUserService(userRepository repository.UserRepository, viper *viper.Viper) UserService {
 	return &userService{
 		userRepository: userRepository,
+		bcryptCost:     viper.GetInt(bcryptCostKey),
 	}
 }
 
 func (s *userService) Login(ctx context.Context, req *common.UserLoginRequest) error {
-	panic("implement me")
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), 15)
+	if err != nil {
+		return err
+	}
+
+	user, err := s.userRepository.GetByEmail(ctx, req.Email)
+	if err != nil {
+		return err
+	}
+
+	if user.Password != string(hashedPassword) {
+		return common.ErrUnauthorized
+	}
+
+	return nil
 }
 
 func (s *userService) Register(ctx context.Context, req *common.UserRegisterRequest) error {
-	panic("implement me")
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), 15)
+	if err != nil {
+		return err
+	}
+
+	user := &model.User{
+		Email:    req.Email,
+		Password: string(hashedPassword),
+	}
+
+	err = s.userRepository.Create(ctx, user)
+	if err != nil {
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			return common.ErrConflict
+		}
+
+		return err
+	}
+
+	return nil
 }
