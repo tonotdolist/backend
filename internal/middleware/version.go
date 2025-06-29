eaa0f4bbf1ecf6c4778ff335c4cdfa0fd4bbea34
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-29 18:00:12 -0400

    fixed incorrect logic for version middleware

--- internal/middleware/version.go
+++ internal/middleware/version.go
@@ -2,12 +2,16 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"strconv"
 	"strings"
 )
 
 func VersionMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Set("version", strings.Split(ctx.Request.URL.Path, "/"))
+		versionString := strings.Replace(strings.Split(ctx.Request.URL.Path, "/")[1], "v", "", 1)
+		version, _ := strconv.ParseUint(versionString, 10, 0)
+
+		ctx.Set("version", uint(version))
 
 		ctx.Next()
 	}
