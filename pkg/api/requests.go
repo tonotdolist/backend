9246ca8cb47040f57d26d5b94f4cf359bcf8d9e2
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-28 23:57:03 -0400

    changed BindJSON function to take a uint version instead of the full ApiVersion interface

--- pkg/api/requests.go
+++ pkg/api/requests.go
@@ -26,17 +26,15 @@ func RegisterRequest[TInternal any, TVersion any](apiVersion uint) {
 	requestToInternal[apiVersion][internalType] = versionedType
 }
 
-func BindJSON(ctx *gin.Context, version ApiVersionHandler, internalType reflect.Type) (interface{}, error) {
-	apiVersion := version.GetApiVersion()
-
-	requestMapping, ok := requestToInternal[apiVersion]
+func BindJSON(ctx *gin.Context, version uint, internalType reflect.Type) (interface{}, error) {
+	requestMapping, ok := requestToInternal[version]
 	if !ok {
-		return nil, fmt.Errorf("api version %d not registered on the versioned request to internal request mapping", apiVersion)
+		return nil, fmt.Errorf("api version %d not registered on the versioned request to internal request mapping", version)
 	}
 
 	requestType, ok := requestMapping[internalType]
 	if !ok {
-		return nil, fmt.Errorf("no versioned request mapping for internal type %q with api version %d", internalType, apiVersion)
+		return nil, fmt.Errorf("no versioned request mapping for internal type %q with api version %d", internalType, version)
 	}
 
 	value := reflect.New(requestType.Elem()).Interface()
