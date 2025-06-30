c072a57b60e761dffadbce478e45f03039930835
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-29 20:25:15 -0400

    made user email unique

--- internal/model/user.go
+++ internal/model/user.go
@@ -12,8 +12,8 @@ func init() {
 type User struct {
 	gorm.Model
 	UserId   string `gorm:"unique;not null"`
+	Email    string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
-	Email    string `gorm:"not null"`
 }
 
 func (u *User) TableName() string {
