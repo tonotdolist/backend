68546214aa9c9e9b4158bd417d7cecd4e453fffb
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-29 21:03:36 -0400

    added logging after request completed to logging middleware

--- internal/middleware/log.go
+++ internal/middleware/log.go
@@ -3,10 +3,12 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
+	"time"
 )
 
 func RequestLogMiddleware(logger zerolog.Logger) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		startTime := time.Now()
 		logger = logger.With().
 			Str("request_url", ctx.Request.URL.String()).
 			Str("request_method", ctx.Request.Method).
@@ -17,5 +19,7 @@ func RequestLogMiddleware(logger zerolog.Logger) gin.HandlerFunc {
 		logger.Info().Msg("request received")
 
 		ctx.Next()
+
+		logger.Info().Dur("time_taken", time.Since(startTime)).Int("status", ctx.Writer.Status()).Msg("request completed")
 	}
 }
