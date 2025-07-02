c4b569b13dc1e0e086b4f48dda3fbf41cb8120b3
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-07-02 18:59:11 -0400

    added string return value to get session method for user id

--- internal/repository/session.go
+++ internal/repository/session.go
@@ -4,7 +4,7 @@ import "context"
 
 type SessionRepository interface {
 	AddSession(ctx context.Context, userId string) (string, error)
-	GetSession(ctx context.Context, sessionId string) error
+	GetSession(ctx context.Context, sessionId string) (string, error)
 	DeleteSession(ctx context.Context, sessionId string) error
 	DeleteAllUserSession(ctx context.Context, userId string) error
 }
