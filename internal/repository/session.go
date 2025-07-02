4018eb1859f00b2b475e2189382b39662f34ca50
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-07-02 18:56:56 -0400

    fixed naming typo for "AddSession"

--- internal/repository/session.go
+++ internal/repository/session.go
@@ -3,7 +3,7 @@ package repository
 import "context"
 
 type SessionRepository interface {
-	AddedSession(ctx context.Context, userId string) (string, error)
+	AddSession(ctx context.Context, userId string) (string, error)
 	GetSession(ctx context.Context, sessionId string)
 	DeleteSession(ctx context.Context, sessionId string)
 	DeleteAllUserSession(ctx context.Context, userId string)
