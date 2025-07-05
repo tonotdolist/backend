be7243d3418879fdca7b65cac94df6bd8beb687a
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-07-05 14:43:28 -0400

    implemented session repo methods

--- internal/repository/session.go
+++ internal/repository/session.go
@@ -1,11 +1,24 @@
 package repository
 
-import "context"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"strconv"
+	"strings"
+	"tonotdolist/common"
+)
+
+type sessionContent struct {
+	userId string
+	expire int64
+}
 
 type SessionRepository interface {
-	AddSession(ctx context.Context, userId string, sessionId string) error
+	AddSession(ctx context.Context, userId string, sessionId string, ttl int64) error
 	GetSession(ctx context.Context, sessionId string) (string, error)
-	DeleteSession(ctx context.Context, sessionId string) error
+	DeleteSession(ctx context.Context, sessionId string, userId string) error
 	DeleteAllUserSession(ctx context.Context, userId string) error
 }
 
@@ -13,20 +26,96 @@ type sessionRepository struct {
 	*Repository
 }
 
-func (r *sessionRepository) AddSession(ctx context.Context, userId string, sessionId string) error {
-	panic("implement me")
+func (r *sessionRepository) AddSession(ctx context.Context, userId string, sessionId string, ttl int64) error {
+	session := &sessionContent{
+		userId: userId,
+		expire: ttl,
+	}
+
+	if err := r.rdb.Set(ctx, sessionId, formatContent(session), 0).Err(); err != nil {
+		return err
+	}
+
+	if err := r.rdb.SAdd(ctx, userId, sessionId).Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *sessionRepository) GetSession(ctx context.Context, sessionId string) (string, error) {
-	panic("implement me")
+	res := r.rdb.Get(ctx, sessionId)
+
+	if err := res.Err(); err != nil {
+		if errors.Is(err, redis.Nil) {
+			return "", common.ErrUnauthorized
+		}
+
+		return "", fmt.Errorf("error fetching user session: %w", err)
+	}
+
+	session, err := parseContent(res.Val())
+	if err != nil {
+		return "", fmt.Errorf("error parsing session data from value: %w", err)
+	}
+
+	return session.userId, nil
 }
 
-func (r *sessionRepository) DeleteSession(ctx context.Context, sessionId string) error {
-	panic("implement me")
+func (r *sessionRepository) DeleteSession(ctx context.Context, sessionId string, userId string) error {
+	newCtx := context.WithoutCancel(ctx)
+	pipeline := r.rdb.Pipeline()
+	cmd1 := pipeline.Del(newCtx, sessionId)
+	cmd2 := pipeline.SRem(newCtx, userId, sessionId)
+	_, err := pipeline.Exec(newCtx)
+	if err != nil {
+		return fmt.Errorf("unable to execute delete session pipeline: %w", err)
+	}
+
+	return errors.Join(cmd1.Err(), cmd2.Err())
 }
 
 func (r *sessionRepository) DeleteAllUserSession(ctx context.Context, userId string) error {
-	panic("implement me")
+	userSessionList, err := r.rdb.SMembers(ctx, userId).Result()
+	if err != nil {
+		return fmt.Errorf("error fetching user session list: %w", err)
+	}
+
+	if len(userSessionList) > 0 {
+		pipeline := r.rdb.Pipeline()
+		cmd1 := pipeline.Del(ctx, userSessionList...)
+		cmd2 := pipeline.Del(ctx, userId)
+
+		_, err = pipeline.Exec(ctx)
+		if err != nil {
+			return fmt.Errorf("unable to execute clear user sessions pipeline: %w", err)
+		}
+
+		return errors.Join(cmd1.Err(), cmd2.Err())
+	}
+
+	return nil
+}
+
+func formatContent(val *sessionContent) string {
+	return fmt.Sprintf("%s:%d", val.userId, val.expire)
+}
+
+func parseContent(content string) (*sessionContent, error) {
+	parts := strings.Split(content, ":")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid format: %s", content)
+	}
+
+	exp, err := strconv.ParseInt(parts[1], 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse expiration timestamp: %w", err)
+	}
+
+	return &sessionContent{
+		userId: parts[0],
+		expire: exp,
+	}, nil
 }
 
 var _ SessionRepository = (*sessionRepository)(nil)
