3d7df62d57f48843dc540d8069cc065c84d99e4b
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-07-05 21:37:54 -0400

    fixed auth middleware still continuing after an issues has been detected

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -5,6 +5,7 @@ import (
 	"tonotdolist/api"
 	"tonotdolist/common"
 	"tonotdolist/internal/service"
+	"tonotdolist/pkg/log"
 )
 
 func AuthMiddleware(userService service.UserService) gin.HandlerFunc {
@@ -12,15 +13,24 @@ func AuthMiddleware(userService service.UserService) gin.HandlerFunc {
 		sessionId := ctx.GetHeader("Authentication")
 		if sessionId == "" {
 			api.HandleResponse(ctx, common.ErrUnauthorized, nil)
+
+			ctx.Abort()
 			return
 		}
 
 		userId, err := userService.GetSession(ctx, sessionId)
 		if err != nil {
+			logger := log.GetLoggerFromContext(ctx)
+			logger.Error().Err(err).Msg("fail to validate session")
 			api.HandleResponse(ctx, err, nil)
+
+			ctx.Abort()
+			return
 		}
 
 		ctx.Set("session_id", sessionId)
 		ctx.Set("user_id", userId)
+
+		ctx.Next()
 	}
 }
