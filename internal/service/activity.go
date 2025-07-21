0a579032ab96d485d7da87d68984b84f82f7ac2f
BuildTools <unconfigured@null.spigotmc.org>
2025-07-21 11:15:06 -0400

    removed using pointer on interface

--- internal/service/activity.go
+++ internal/service/activity.go
@@ -6,9 +6,9 @@ type ActivityService interface {
 }
 
 type activityService struct {
-	activityRepository *repository.ActivityRepository
+	activityRepository repository.ActivityRepository
 }
 
-func NewActivityService(activityRepository *repository.ActivityRepository) ActivityService {
+func NewActivityService(activityRepository repository.ActivityRepository) ActivityService {
 	return &activityService{activityRepository: activityRepository}
 }
