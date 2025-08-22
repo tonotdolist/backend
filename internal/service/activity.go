fb2abbf189d309ce337ea992918d08d0d6795e5e
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-08-22 00:36:48 -0400

    implemented create function in activity service

--- internal/service/activity.go
+++ internal/service/activity.go
@@ -1,10 +1,21 @@
 package service
 
-import "tonotdolist/internal/repository"
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"tonotdolist/common"
+	"tonotdolist/internal/model"
+	"tonotdolist/internal/repository"
+)
 
 type ActivityService interface {
+	Create(ctx context.Context, UserId string, request *common.ActivityCreateRequest) error
+	Update(ctx context.Context, UserId string, request *common.ActivityUpdateRequest) error
+	Delete(ctx context.Context, UserId string, request *common.ActivityDeleteRequest) error
+	//FetchByCount(ctx context.Context, UserId string, request *common.ActivityFetchByCountRequest) (error,)
+	//FetchByTimeRange(ctx context.Context, UserId string, request *common.ActivityFetchByTimeRangeRequest) (error, []*model.Activity)
 }
-
 type activityService struct {
 	activityRepository repository.ActivityRepository
 }
@@ -12,3 +23,49 @@ type activityService struct {
 func NewActivityService(activityRepository repository.ActivityRepository) ActivityService {
 	return &activityService{activityRepository: activityRepository}
 }
+
+func (a activityService) Create(ctx context.Context, UserId string, request *common.ActivityCreateRequest) error {
+	activityId, err := uuid.NewUUID()
+
+	if err != nil {
+		return fmt.Errorf("error generating activity uuid: %w", err)
+	}
+
+	activityModel := &model.Activity{
+		ActivityId:  activityId.String(),
+		UserId:      UserId,
+		Type:        request.Type,
+		Name:        request.Name,
+		Priority:    request.Priority,
+		Description: request.Description,
+		Location:    request.Location,
+		Date:        request.Date,
+	}
+
+	err = a.activityRepository.CreateActivity(ctx, activityModel)
+	if err != nil {
+		return fmt.Errorf("error creating activity in db:  %w", err)
+	}
+
+	return nil
+}
+
+func (a activityService) Update(ctx context.Context, UserId string, request *common.ActivityUpdateRequest) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a activityService) Delete(ctx context.Context, UserId string, request *common.ActivityDeleteRequest) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+//func (a activityService) FetchByCount(ctx context.Context, UserId string, request *common.ActivityFetchByCountRequest) {
+//	//TODO implement me
+//	panic("implement me")
+//}
+//
+//func (a activityService) FetchByTimeRange(ctx context.Context, UserId string, request *common.ActivityFetchByTimeRangeRequest) {
+//	//TODO implement me
+//	panic("implement me")
+//}
