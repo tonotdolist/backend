6601dcdbfa0c1decc1858aecb05be7ca8d63f987
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-08-23 23:23:41 -0400

    moved bind json logics to api package and added versioned response system

--- api/api.go
+++ api/api.go
@@ -2,14 +2,15 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"reflect"
 	"tonotdolist/common"
 	"tonotdolist/pkg/api"
 	"tonotdolist/pkg/log"
 )
 
-func HandleResponse(ctx *gin.Context, err error, data interface{}) {
-	if data == nil {
-		data = map[string]string{}
+func HandleResponse(ctx *gin.Context, err error, resp interface{}) {
+	if resp == nil {
+		resp = map[string]string{}
 	}
 
 	apiVersion := ctx.GetUint(api.ApiVersionContextKey)
@@ -26,7 +27,26 @@ func HandleResponse(ctx *gin.Context, err error, data interface{}) {
 		return
 	}
 
-	httpCode, resp := apiHandler.HandleResponse(api.GetError(apiHandler, err), data)
+	versionedResp, err := api.GetResponse(resp, apiVersion)
+	if err != nil {
+		logger := log.GetLoggerFromContext(ctx)
+		logger.Error().Msgf("unable to get versioned response: %s", err.Error())
+		ctx.JSON(500, map[string]string{})
+	}
+
+	httpCode, resp := apiHandler.HandleResponse(api.GetError(apiHandler, err), versionedResp)
 
 	ctx.JSON(httpCode, resp)
+}
+
+func BindJSON(ctx *gin.Context, internalType reflect.Type, version uint) (interface{}, error) {
+	value, err := api.GetRequest(internalType, version)
+	if err != nil {
+		return nil, err
+	}
+	if err = ctx.ShouldBindJSON(value); err != nil {
+		return nil, common.ErrBadRequest
+	}
+
+	return value.ToInternalRequest(), nil
 }
--- pkg/api/requests.go
+++ pkg/api/requests.go
@@ -2,9 +2,7 @@ package api
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"reflect"
-	"tonotdolist/common"
 )
 
 var requestToInternal = make(map[uint]map[reflect.Type]reflect.Type)
@@ -15,7 +13,7 @@ type VersionedRequest interface {
 }
 
 type VersionedResponse interface {
-	FromInternalResponse() interface{}
+	FromInternalResponse(data interface{}) interface{}
 }
 
 func RegisterRequest[TInternal any, TVersion any](apiVersion uint) {
@@ -55,8 +53,7 @@ func RegisterResponse[TInternal any, TVersion any](apiVersion uint) {
 	internalToResponse[apiVersion][versionedType] = internalType
 }
 
-func BindJSON(ctx *gin.Context, internalType reflect.Type) (interface{}, error) {
-	version := ctx.GetUint(ApiVersionContextKey)
+func GetRequest(internalType reflect.Type, version uint) (VersionedRequest, error) {
 	requestMapping, ok := requestToInternal[version]
 	if !ok {
 		return nil, fmt.Errorf("api version %d not registered on the versioned request to internal request mapping", version)
@@ -70,9 +67,24 @@ func BindJSON(ctx *gin.Context, internalType reflect.Type) (interface{}, error) 
 	value := reflect.New(requestType.Elem()).Interface()
 	versionedRequest, _ := value.(VersionedRequest)
 
-	if err := ctx.ShouldBindJSON(value); err != nil {
-		return nil, common.ErrBadRequest
+	return versionedRequest, nil
+}
+
+func GetResponse(resp interface{}, version uint) (interface{}, error) {
+	responseMapping, ok := internalToResponse[version]
+	if !ok {
+		return nil, fmt.Errorf("api version %d not registered on the versioned request to internal request mapping", version)
 	}
 
-	return versionedRequest.ToInternalRequest(), nil
+	incomingType := reflect.TypeOf(resp)
+
+	responseType, ok := responseMapping[incomingType]
+	if !ok {
+		return nil, fmt.Errorf("no versioned response mapping for internal type %q with api version %d", incomingType, version)
+	}
+
+	value := reflect.New(responseType.Elem()).Interface()
+	versionedRequest, _ := value.(VersionedResponse)
+
+	return versionedRequest.FromInternalResponse(resp), nil
 }
