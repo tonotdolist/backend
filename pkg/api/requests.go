982bb3844a881cc97fe40f5efc921d258d90ed6c
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-08-23 22:51:51 -0400

    added versioned request system for request response and moved interface implement check to register method

--- pkg/api/requests.go
+++ pkg/api/requests.go
@@ -8,9 +8,14 @@ import (
 )
 
 var requestToInternal = make(map[uint]map[reflect.Type]reflect.Type)
+var internalToResponse = make(map[uint]map[reflect.Type]reflect.Type)
 
 type VersionedRequest interface {
 	ToInternalRequest() interface{}
+}
+
+type VersionedResponse interface {
+	FromInternalResponse() interface{}
 }
 
 func RegisterRequest[TInternal any, TVersion any](apiVersion uint) {
@@ -24,7 +29,30 @@ func RegisterRequest[TInternal any, TVersion any](apiVersion uint) {
 	internalType := reflect.TypeOf((*TInternal)(nil))
 	versionedType := reflect.TypeOf((*TVersion)(nil))
 
+	_, ok := versionedType.(VersionedRequest)
+	if !ok {
+		panic(fmt.Sprintf("%T does not implement VersionedRequest interface", versionedType))
+	}
+
 	requestToInternal[apiVersion][internalType] = versionedType
+}
+
+func RegisterResponse[TInternal any, TVersion any](apiVersion uint) {
+	responseMapping := internalToResponse[apiVersion]
+
+	if responseMapping == nil {
+		responseMapping = make(map[reflect.Type]reflect.Type)
+		internalToResponse[apiVersion] = responseMapping
+	}
+
+	internalType := reflect.TypeOf((*TInternal)(nil))
+	versionedType := reflect.TypeOf((*TVersion)(nil))
+	_, ok := versionedType.(VersionedResponse)
+	if !ok {
+		panic(fmt.Sprintf("%T does not implement VersionedResponse interface", versionedType))
+	}
+
+	internalToResponse[apiVersion][versionedType] = internalType
 }
 
 func BindJSON(ctx *gin.Context, internalType reflect.Type) (interface{}, error) {
@@ -40,10 +68,7 @@ func BindJSON(ctx *gin.Context, internalType reflect.Type) (interface{}, error) 
 	}
 
 	value := reflect.New(requestType.Elem()).Interface()
-	versionedRequest, ok := value.(VersionedRequest)
-	if !ok {
-		return nil, fmt.Errorf("type %T does not implement VersionedRequest", value)
-	}
+	versionedRequest, _ := value.(VersionedRequest)
 
 	if err := ctx.ShouldBindJSON(value); err != nil {
 		return nil, common.ErrBadRequest
