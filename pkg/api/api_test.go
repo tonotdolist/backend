8562cd2562baa59ab2e0d1492fe3d5bedb8ba0a0
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-08-26 17:49:21 -0400

    added response test

--- pkg/api/api_test.go
+++ pkg/api/api_test.go
@@ -13,8 +13,15 @@ type testVersionedStruct struct {
 	fieldd int16
 }
 
+type testVersionedResStruct struct {
+	fieldd int16
+}
+
 func (tis *testVersionedStruct) ToInternalRequest() interface{} {
 	return nil
+}
+
+func (tis *testVersionedResStruct) FromInternalResponse(_ interface{}) {
 }
 
 func TestRequestMapping(t *testing.T) {
@@ -24,6 +31,7 @@ func TestRequestMapping(t *testing.T) {
 	req, err := GetRequest(internalTyp, version)
 	if err != nil {
 		t.Errorf("error getting request: %v", err)
+		return
 	}
 
 	gotType := reflect.TypeOf(req)
@@ -33,4 +41,21 @@ func TestRequestMapping(t *testing.T) {
 	}
 }
 
+func TestResponseMapping(t *testing.T) {
+	var version uint = 0
+	RegisterResponse[testInternalStruct, testVersionedResStruct](version)
+	req, err := GetResponse(testInternalStruct{}, version)
+	if err != nil {
+		t.Errorf("error getting response: %v", err)
+		return
+	}
+
+	gotType := reflect.TypeOf(req)
+	expectedTyp := reflect.TypeOf((*testVersionedResStruct)(nil))
+	if gotType != expectedTyp {
+		t.Errorf("incorrect GetResponse returned type. expected: %s, got: %s", expectedTyp, gotType)
+	}
+}
+
 var _ VersionedRequest = (*testVersionedStruct)(nil)
+var _ VersionedResponse = (*testVersionedResStruct)(nil)
