33a9029ecbc4788c9e2fdd3cc6ac02ffbd15800c
nolifejavadeveloper <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-09-28 00:30:22 -0400

    added test for config.go

---
+++ pkg/config/config_test.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestConfigLoad(t *testing.T) {
+	const (
+		key    = "key"
+		badKey = "bkey"
+		value  = "value"
+
+		length = 1
+	)
+	RegisterRequiredKey(key)
+
+	t.Run("SuccessfulConfig", func(t *testing.T) {
+		const (
+			data = key + ": " + value + "g\n"
+		)
+
+		conf, err := NewConfig(strings.NewReader(data), "yaml")
+
+		assert.Nil(t, err, "Unexpected error when initializing config.")
+		assert.Equal(t, length, len(conf.AllKeys()), "The length of the config keys do not match.")
+		assert.Equal(t, value, conf.Get(key), "The value of %s in the config does not match the expected value.", key)
+	})
+
+	t.Run("ValidateKeyFail", func(t *testing.T) {
+		const (
+			data = badKey + ": " + value + "\n"
+		)
+
+		_, err := NewConfig(strings.NewReader(data), "yaml")
+		assert.NotNil(t, err, "Expected error caused by missing required key.")
+	})
+
+	t.Run("BadConfigFormat", func(t *testing.T) {
+		const (
+			data = key + " " + value + "\n"
+		)
+
+		_, err := NewConfig(strings.NewReader(data), "yaml")
+
+		assert.NotNil(t, err, "Expected error caused by malformatted YAML config.")
+	})
+}
